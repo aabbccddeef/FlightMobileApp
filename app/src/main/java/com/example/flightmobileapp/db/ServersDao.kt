b9c286338e661e025e4f91665c44b3edb027d93b
menimen <menash9288@gmail.com>
2020-06-16 20:27:32 +0300

    fixed query in ServerDao class

--- app/src/main/java/com/example/flightmobileapp/db/ServersDao.kt
+++ app/src/main/java/com/example/flightmobileapp/db/ServersDao.kt
@@ -9,7 +9,7 @@ import androidx.room.Query
 interface ServersDao {
     // https://localhost:44383/
 
-    @Query("select url from servers_table limit 5")
+    @Query("select url from servers_table order by id desc limit 5")
     fun getLastFive(): LiveData<List<DatabaseEntities.Server>>
 
     @Insert
